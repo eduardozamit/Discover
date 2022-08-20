b8318741ba104f72ff698957fb548c88a7c06858
Eduardo Zamit <85138084+eduardozamit@users.noreply.github.com>
2022-08-19 22:11:22 -0300

    Delete Guia estelar JavaScript directory

--- Guia estelar JavaScript/Manipulando dados/TypeConversion-TypeCoersion.js
+++
@@ -1,9 +0,0 @@
-// Type conversion (typecasting) vs type coersion
-
-//* Alteração de um tipo de dado para outro tipo
-
-//Neste exemplo o JS usou da coersion para trocar o dado do numero 5 de number para string.
-console.log('9' + 5)
-
-//Já neste exeplo EU utilizei do conversion para trocar o dado do numero 9 de String para Number
-console.log(Number('9') + 5)
