4b1f4be8783f17dcdb775f7a6694c54d120b2397
Eduardo Zamit <85138084+eduardozamit@users.noreply.github.com>
2022-09-01 19:55:17 -0300

    01/09/22

---
+++ Programação Funcional/imperativa-declarativa.js
@@ -0,0 +1,13 @@
+// Função que eleva o número ao quadrado
+
+// Imperativo: Faça da seguinte forma (dar ordens)
+let number = 2
+
+function square() {
+    return number * number
+}
+
+number = square()
+
+// Declarativa: O que fazer e nao como fazer (a função ta ai)
+const square = n => n * n
